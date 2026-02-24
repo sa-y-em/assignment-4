663e0c5fbce98b28b3ecb7056ca253e754cbab25
sa-y-em <sayemmir100@gmail.com>
2026-02-24 21:25:52 +0600

    toggle added

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const allCards = document.getElementById('all-cards');
 let interviewArray = [];
 let rejectedArray = [];
 
-// let total.innerText =
-console.log()
+//job number
 
 function jobNumber(){
   total.innerText =  allCards.children.length
@@ -15,3 +14,26 @@ function jobNumber(){
   rejected.innerText = rejectedArray.length;
 }
 jobNumber() //calling all jobs ,interview and rejected numbers
+
+//toggling part
+const allButton = document.getElementById('all-button');
+const interviewButton = document.getElementById('interview-button');
+const rejectedButton = document.getElementById('rejected-button');
+
+function toggle(id){
+    //removing black
+   allButton.classList.remove('bg-black','text-white');
+   interviewButton.classList.remove('bg-black','text-white');
+   rejectedButton.classList.remove('bg-black','text-white');
+   //adding white
+   
+    allButton.classList.add('bg-white', 'text-black');
+    interviewButton.classList.add('bg-white', 'text-black');
+    rejectedButton.classList.add('bg-white', 'text-black');
+    //adding black to clicked button
+    const clicked = document.getElementById(id);
+    clicked.classList.remove('bg-white','text-black');
+    clicked.classList.add('bg-black','text-white');
+
+
+}
